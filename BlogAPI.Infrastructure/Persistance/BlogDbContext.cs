817160472c2df5e7c0162e7f8da5dcc510b65835
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-03 03:39:12 +0500

    Configurations added

---
+++ BlogAPI.Infrastructure/Persistance/BlogDbContext.cs
@@ -0,0 +1,49 @@
+﻿using BlogAPI.Domain.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Infrastructure.Persistance
+{
+    public class BlogDbContext:DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) 
+            : base(options) 
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>()
+                .HasMany(x => x.Permissions)
+                .WithMany(x => x.Roles)
+                .UsingEntity<RolePermission>();
+
+
+            modelBuilder.Entity<BlogPost>()
+                .HasOne(b => b.User)
+                .WithOne(u => u.BlogPost)
+                .HasForeignKey<BlogPost>(b => b.User.Id);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+
+
+    }
+}
